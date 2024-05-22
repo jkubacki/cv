d28a6bfe2bfda5165f1122d5cbe6ef76f39c7fd6
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-22 22:53:10 +0200

    Update About.tsx

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <section className="About flex flex-col items-center gap-5">
+    <section className="About flex flex-col items-center gap-5 print:my-4">
       <h2 className="text-2xl font-bold print:hidden">About me</h2>
       <div className="flex flex-col items-center gap-4">
         <p className="text-md">
