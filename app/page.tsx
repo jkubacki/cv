b71baf4759ee1750677ad6c1765ddb8dffcf44a0
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-22 13:55:48 +0200

    Add link to the demo app

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Languages from '@/components/sections/Languages/Languages';
 import Interests from '@/components/sections/Interests/Interests';
 import PrintButton from '@/components/PrintButton';
 import DownloadPDFButton from '@/components/DownlaodPDFButton';
+import { GitHubIcon } from '@/components/icons/GitHubIcon';
 
 export default function Home() {
   return (
@@ -26,10 +27,23 @@ export default function Home() {
         <DownloadPDFButton />
         <PrintButton />
       </div>
-      <footer className="text-center text-sm mb-3 print:hidden">
-        <p>
+      <footer className="text-center text-sm mb-3 print:hidden flex flex-col gap-3 items-center">
+        <div className="flex items-center gap-1">
           Made with ❤️ using TypeScript, React, Next.js, Tailwind CSS and shadcn/ui
-        </p>
+          <a href="https://github.com/jkubacki/cv" target="_blank"><GitHubIcon className="w-4 h-4" /></a>
+        </div>
+        <div className="flex items-center gap-1">
+          Hosted on Amazon EKS
+        </div>
+        <div className="flex items-center gap-1">
+          Check out my demo app at
+          <div className="underline">
+            <a href="https://jobs.jakubkubacki.com" target="_blank">
+              https://jobs.jakubkubacki.com
+            </a>
+          </div>
+          <a href="https://github.com/jkubacki/jobs-app" target="_blank"><GitHubIcon className="w-4 h-4" /></a>
+        </div>
       </footer>
     </main>
   );
