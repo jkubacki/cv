94070c0a9356c96731bbcb166b1e69652a09357b
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:28:32 +0200

    Update PrintSubSkill.tsx

--- components/sections/Skills/Skill/SubSkills/PrintSubSkill/PrintSubSkill.tsx
+++ components/sections/Skills/Skill/SubSkills/PrintSubSkill/PrintSubSkill.tsx
@@ -4,7 +4,7 @@ export default function PrintSubSkill({ skill }: { skill: ISkill}) {
 
   return (
     <div className="text-sm">
-        <span>{skill.name}</span> - <span>{skill.subSkills && skill.subSkills.map((subSkill, index) => subSkill.name).join(', ') }</span>
+        <span>{skill.name}</span> { skill.subSkills && skill.subSkills.length > 0 && (<>-</>)} <span>{skill.subSkills && skill.subSkills.map((subSkill, index) => subSkill.name).join(', ') }</span>
     </div>
   )
 }
