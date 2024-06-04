a7adec19f509f4e144b744ca5d3fcb6e709ab6b3
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:36:50 +0200

    Update DOCKER_SKILLS.ts

--- lib/data/DEV_OPS_SKILLS/DOCKER_SKILLS.ts
+++ lib/data/DEV_OPS_SKILLS/DOCKER_SKILLS.ts
@@ -3,7 +3,7 @@ import { ISkill } from '@/lib/types/ISkill';
 export const DOCKER_SKILLS: ISkill = {
   name: 'Docker',
   since: 2016,
-  description: "",
+  description: "I use Docker on a daily basis in development and production environments.",
   subSkills: [
   ]
 }
