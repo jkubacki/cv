206b4679f8bd5bfdd77f7c162ed7367803742ac5
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:25:37 +0200

    Better spacing for print

--- components/sections/Skills/Skill/Skill.tsx
+++ components/sections/Skills/Skill/Skill.tsx
@@ -4,7 +4,7 @@ import { ISkill } from '@/lib/types/ISkill';
 
 export default function Skill({ skill }: { skill: ISkill}) {
   return (
-    <div id={skill.name} className="Skill w-full flex flex-col gap-3 mb-3">
+    <div id={skill.name} className="Skill w-full flex flex-col gap-3 mb-3 print:gap-1 print:mb-0">
       <h3 className="font-bold">{skill.name} - {sinceToString(skill.since)}</h3>
       <div className="text-sm print:hidden">{skill.description}</div>
       {skill.subSkills && <SubSkills skills={skill.subSkills} />}
