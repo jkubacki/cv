8bdd40fe55f1187cc0fb3d0d21418e7242d8473c
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:04:46 +0200

    Update About.tsx

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -11,7 +11,7 @@ export default function About() {
       <h2 className="text-2xl font-bold print:hidden">About me</h2>
       <div className="flex flex-col items-center gap-4">
         <p className="text-md">
-          With 14 years of experience in web development, I'm passionate about crafting high-quality, maintainable, and scalable software using the best tools for the job.
+          I'm passionate about crafting high-quality, maintainable, and scalable software using the best tools for the job.
           I thrive in remote, collaborative team environments and have successfully led multiple projects.
           I'm comfortable taking full ownership of a product, ensuring its success from start to finish.
         </p>
