d545d3b7fd5fa16edc52ae1fa884bf8c8928ae2a
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:23:48 +0200

    Update TECH_LEAD_SKILLS.ts

--- lib/data/LEADERSHIP_SKILLS/TECH_LEAD_SKILLS.ts
+++ lib/data/LEADERSHIP_SKILLS/TECH_LEAD_SKILLS.ts
@@ -5,13 +5,9 @@ export const TECH_LEAD_SKILLS: ISkill = {
   since: 2016,
   description: 'I have served as a technical lead for multiple projects over several years, directing their technical direction. This included code reviews, pair programming, and mentoring. Additionally, I oversaw project quality and managed technical debt. I also acted as the primary point of contact for clients alongside the Project Manager.',
   subSkills: [
-    { name: 'Ownership' },
-    { name: 'Software architecture' },
-    { name: 'Scoping sessions' },
+    { name: 'Full ownership' },
+    { name: 'Architecture' },
     { name: 'Code reviews' },
-    { name: 'Team building' },
-    { name: 'Cross-functional teams' },
-    { name: 'Roadmaps, vision, goals' },
     { name: 'Agile' },
   ]
 }
