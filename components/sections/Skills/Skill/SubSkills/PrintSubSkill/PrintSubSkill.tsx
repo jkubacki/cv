492ec37ba22973dc37ee998c85ccf77126f8e7a2
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-04 16:24:08 +0200

    Squash print subskills

--- components/sections/Skills/Skill/SubSkills/PrintSubSkill/PrintSubSkill.tsx
+++ components/sections/Skills/Skill/SubSkills/PrintSubSkill/PrintSubSkill.tsx
@@ -1,20 +1,10 @@
-import { sinceToString } from '@/lib/sinceToString';
-import { sinceToYears } from '@/lib/sinceToYears';
 import { ISkill } from '@/lib/types/ISkill';
 
 export default function PrintSubSkill({ skill }: { skill: ISkill}) {
-  const years = sinceToYears(skill.since)
-  const yearsOfExperience = sinceToString(skill.since)
 
   return (
-    <div className="flex flex-col gap-1">
-      <div className='text-sm'>
-        <span className='font-bold'>{skill.name}</span>
-        { years !== 0 && <span> - {yearsOfExperience}</span>}
-      </div>
-      <div className='text-sm'>
-        {skill.subSkills && skill.subSkills.map((subSkill, index) => subSkill.name).join(', ') }
-      </div>
+    <div className="text-sm">
+        <span>{skill.name}</span> - <span>{skill.subSkills && skill.subSkills.map((subSkill, index) => subSkill.name).join(', ') }</span>
     </div>
   )
 }
