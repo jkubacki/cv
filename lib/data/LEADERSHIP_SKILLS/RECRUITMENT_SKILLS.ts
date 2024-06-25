f283521b01f149f40502ca9af0e89e4bdad57485
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-25 11:05:56 +0200

    Add skills

--- lib/data/LEADERSHIP_SKILLS/RECRUITMENT_SKILLS.ts
+++ lib/data/LEADERSHIP_SKILLS/RECRUITMENT_SKILLS.ts
@@ -5,6 +5,7 @@ export const RECRUITMENT_SKILLS: ISkill = {
   since: 2016,
   description: 'I have been involved in the recruitment process for several years, conducting interviews. I have also been responsible for improving and unifying recruitment process.',
   subSkills: [
+    { name: 'Talent sourcing' },
     { name: 'Interviewing' },
     { name: 'Recruitment process' },
   ]
--- lib/data/LEADERSHIP_SKILLS/TECH_LEAD_SKILLS.ts
+++ lib/data/LEADERSHIP_SKILLS/TECH_LEAD_SKILLS.ts
@@ -6,6 +6,7 @@ export const TECH_LEAD_SKILLS: ISkill = {
   description: 'I have served as a technical lead for multiple projects over several years, directing their technical direction. This included code reviews, pair programming, and mentoring. Additionally, I oversaw project quality and managed technical debt. I also acted as the primary point of contact for clients alongside the Project Manager.',
   subSkills: [
     { name: 'Ownership' },
+    { name: 'System Design' },
     { name: 'Architecture' },
     { name: 'Code reviews' },
     { name: 'Agile' },
